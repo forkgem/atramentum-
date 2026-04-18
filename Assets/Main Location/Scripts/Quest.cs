c03782e7e4bfea6d1a0009326c0340f7cf412463
forkgem <s31151@pjwstk.edu.pl>
2026-04-18 02:22:04 +0200

    QuestScriptUnfinished
    Scriptable quest unfinished( we dont have game manager yet)

---
+++ Assets/Main Location/Scripts/Quest.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+[CreateAssetMenu(menuName = "Quests/Quest")]
+public class Quest : ScriptableObject
+{
+    public string questID;
+    public string questName;
+    public string description;
+    public List<QuestObjective> objectives;
+
+    private void OnValidate()
+    {
+        if (string.IsNullOrEmpty(questID))
+        {
+            questID = questName + Guid.NewGuid().ToString();
+        }
+    }
+
+    [System.Serializable]
+    public class QuestObjective
+    {
+        public string objectiveID; //Match with ID that you need to collect, enemy ID, etc.
+        public string description;
+        public ObjectiveType type;
+        public int requiredAmount;
+        public int currentAmount;
+
+        public bool IsCompeted => currentAmount >= requiredAmount;
+    }
+
+    public enum ObjectiveType { CollectItem, TalkNPC, Custom }
+
+
+    [System.Serializable]
+
+    public class QuestProgress
+    {
+        public Quest quest;
+        public List<QuestObjective> objectives;
+
+        public QuestProgress (Quest quest)
+        {
+            this.quest = quest;
+            objectives = new List<QuestObjective>();
+
+            //Deep copy to avoid modifying original
+
+            foreach (var obj in quest.objectives)
+            {
+                objectives.Add(new QuestObjective
+                {
+                    objectiveID = obj.objectiveID,
+                    description = obj.description,
+                    type = obj.type,
+                    requiredAmount = obj.requiredAmount,
+                    currentAmount = 0
+                });
+            }
+        }
+
+        public bool IsCompleted => objectives.TrueForAll (o => o.IsCompeted);
+        public string QuestID => quest.questID;
+    }
+
+}
